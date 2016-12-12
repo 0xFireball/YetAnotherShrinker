c3121176f74372a7966343816c1ad171fa9e89a9
0xFireball <0xFireball@outlook.com>
2016-12-12 11:33:44 -0800

    add vue material

--- YetAnotherShrinker/src/YetAnotherShrinker/ClientApp/main.js
+++ YetAnotherShrinker/src/YetAnotherShrinker/ClientApp/main.js
@@ -7,6 +7,19 @@ import App from './App'
 import './assets/styles/normalize.css'
 import './assets/styles/ffskeleton.css'
 
+// material
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.css'
+
+Vue.use(VueMaterial)
+
+Vue.material.theme.registerAll({
+  apptheme: {
+    primary: 'teal',
+    accent: 'cyan'
+  }
+})
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
