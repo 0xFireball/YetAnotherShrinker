983355166b4ad2073db5dc611bccc38a3492dc4b
0xFireball <0xFireball@outlook.com>
2016-12-12 11:41:33 -0800

    add project dependencies

--- YetAnotherShrinker/src/YetAnotherShrinker/ClientApp/App.vue
+++ YetAnotherShrinker/src/YetAnotherShrinker/ClientApp/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <!--<img src="./assets/logo.png">-->
+    <h1>{{ appTitle }}</h1>
     <hello></hello>
   </div>
 </template>
@@ -10,6 +11,11 @@ import Hello from './components/Hello'
 
 export default {
   name: 'app',
+  data: function () {
+    return {
+      appTitle: 'YetAnotherShrinker'
+    }
+  },
   components: {
     Hello
   }
@@ -17,12 +23,12 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
