526e6f0a7bbed259531f1833c64647a1ec2aaa2f
0xFireball <0xFireball@outlook.com>
2016-12-12 12:00:33 -0800

    style homepage

--- YetAnotherShrinker/src/YetAnotherShrinker/ClientApp/App.vue
+++ YetAnotherShrinker/src/YetAnotherShrinker/ClientApp/App.vue
@@ -1,13 +1,12 @@
 <template>
   <div id="app">
     <!--<img src="./assets/logo.png">-->
-    <h1>{{ appTitle }}</h1>
-    <hello></hello>
+    <intro></intro>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
+import Intro from './components/Intro'
 
 export default {
   name: 'app',
@@ -17,18 +16,20 @@ export default {
     }
   },
   components: {
-    Hello
+    Intro
   }
 }
 </script>
 
 <style>
   #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
     color: #2c3e50;
     margin-top: 60px;
+  }
+  body {
+    background: #f7f7f7;
   }
 </style>
---
+++ YetAnotherShrinker/src/YetAnotherShrinker/ClientApp/components/Intro.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="intro" v-md-theme="'apptheme'">
+    <h1 class="app-title">{{ appTitle }}</h1>
+
+    <div class="container">
+      <div class="row">
+        <div class="eight columns offset-by-two">
+          <h4>Shrink a URL</h4>
+          <form novalidate @submit.stop.prevent="submit">
+            <md-input-container>
+              <label>Initial value</label>
+              <md-input v-model="initialValue"></md-input>
+            </md-input-container>
+
+            <md-input-container>
+              <label>With label</label>
+              <md-input placeholder="My nice placeholder"></md-input>
+            </md-input-container>
+
+            <md-input-container md-inline>
+              <label>Inline field</label>
+              <md-input></md-input>
+            </md-input-container>
+
+            <md-input-container>
+              <label>Number</label>
+              <md-input type="number"></md-input>
+            </md-input-container>
+
+            <md-input-container>
+              <label>Textarea</label>
+              <md-textarea></md-textarea>
+            </md-input-container>
+
+            <md-input-container>
+              <label>Disabled</label>
+              <md-input disabled></md-input>
+            </md-input-container>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'intro',
+  data () {
+    return {
+        appTitle: 'YetAnotherShrinker',
+        msgData: ''
+    }
+  },
+  mounted: function () {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .intro {
+    margin-top: 15%;
+  }
+  
+  .app-title {
+    font-size: 6rem;
+    letter-spacing: 0.1rem;
+    font-weight: 300;
+  }
+</style>
