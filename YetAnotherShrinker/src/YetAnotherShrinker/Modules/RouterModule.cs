f6aa4425d009bb6e0e9922994675f49951210fb7
0xFireball <0xFireball@outlook.com>
2016-12-12 16:23:02 -0800

    add router module

---
+++ YetAnotherShrinker/src/YetAnotherShrinker/Modules/RouterModule.cs
@@ -0,0 +1,24 @@
+﻿using Nancy;
+using YetAnotherShrinker.Services.Shrinker;
+
+namespace YetAnotherShrinker.Modules
+{
+    public class RouterModule : NancyModule
+    {
+        public RouterModule() : base("/r")
+        {
+            Get("{su}", async args =>
+            {
+                var targetUrl = await ShrinkerService.RetrieveShrunkUrlAsync((string)args.su);
+                if (targetUrl == null)
+                {
+                    return new Response().WithStatusCode(HttpStatusCode.NotFound);
+                }
+                // Log request, analytics, etc.
+
+                // Redirect user
+                return Response.AsRedirect(targetUrl.Target);
+            });
+        }
+    }
+}
