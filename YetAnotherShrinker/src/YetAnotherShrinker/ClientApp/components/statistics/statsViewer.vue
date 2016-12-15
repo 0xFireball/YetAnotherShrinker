724e26f914610183ee7c439d6ae32cf567e3e3a5
0xFireball <0xFireball@outlook.com>
2016-12-15 14:33:25 -0800

    clean up statsViewer source

--- YetAnotherShrinker/src/YetAnotherShrinker/ClientApp/components/statistics/statsViewer.vue
+++ YetAnotherShrinker/src/YetAnotherShrinker/ClientApp/components/statistics/statsViewer.vue
@@ -59,18 +59,6 @@
       let vm = this
       // get shrink route
       let shrinkRoute = vm.$route.params.sroute
-      // use dummy data
-      // this.stats.data = {
-      //   labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-      //   datasets: [
-      //     {
-      //       label: 'GitHub Commits',
-      //       backgroundColor: '#f87979',
-      //       data: [40, 20, 12, 39, 10, 40, 39, 80, 40, 20, 12, 11]
-      //     }
-      //   ]
-      // }
-
       // get data from server
       axios.get('/x/stats/' + shrinkRoute,
         axiosRequestConfig)
@@ -89,7 +77,7 @@
                 }
               ]
             }
-            console.log(analyticsBundle.daySortedEvents)
+            // console.log(analyticsBundle.daySortedEvents)
             var ctx = "trafficChart"
             var myLineChart = Chart.Line(ctx, {
               data: vm.stats.data,
